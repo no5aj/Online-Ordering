924461f49ae949537b63cf8be4a3a6cda19d5802
evegorov <evegorov@mera.ru>
2017-06-21 16:30:25 +0300

    WOMA-244 Publish html coverage report for every auto tests run

--- js/mainTestAuto.js
+++ js/mainTestAuto.js
@@ -137,6 +137,7 @@ require(['app', 'js/utest/data/Settings'], function(app, settings_data) {
                        jasmine.BlanketReporter.prototype.jasmineDone.apply(this,arguments);
                        var cover = _blanket.getCovarageTotals()
                        console.log( "Total coverage: " + ((cover.numberOfFilesCovered * 100) / cover.totalSmts).toFixed(2) + "%" );
+                       create_html_report();
                     }
 
                      var reporterCurrentSpec = {
@@ -157,6 +158,38 @@ require(['app', 'js/utest/data/Settings'], function(app, settings_data) {
                         });
                     });
                 });
+
+                function create_html_report() {
+                    var doc_template = '<html> \
+                        <head> \
+                            <link rel="shortcut icon" type="image/png" href="js/utest/jasmine/lib/jasmine2/jasmine_favicon.png"> \
+                            <link rel="stylesheet" type="text/css" href="js/utest/jasmine/lib/jasmine2/jasmine.css"> \
+                            <style>{{insert_styles}}</style> \
+                            <script>{{insert_script}} </script> \
+                        </head> \
+                        <body> <div id="blanket-main"> {{insert_report}} </div> </body> \
+                        </html>';
+
+                    var output,
+                        reportHTML = $('#blanket-main').html(),
+                        styles = "#blanket-main {margin:2px;background:#EEE;color:#333;clear:both;font-family:'Helvetica Neue Light', 'HelveticaNeue-Light', 'Helvetica Neue', Calibri, Helvetica, Arial, sans-serif; font-size:17px;} #blanket-main a {color:#333;text-decoration:none;}  #blanket-main a:hover {text-decoration:underline;} .blanket {margin:0;padding:5px;clear:both;border-bottom: 1px solid #FFFFFF;} .bl-error {color:red;}.bl-success {color:#5E7D00;} .bl-file{width:auto;} .bl-cl{float:left;} .blanket div.rs {margin-left:50px; width:150px; float:right} .bl-nb {padding-right:10px;} #blanket-main a.bl-logo {color: #EB1764;cursor: pointer;font-weight: bold;text-decoration: none} .bl-source{ overflow-x:scroll; background-color: #FFFFFF; border: 1px solid #CBCBCB; color: #363636; margin: 25px 20px; width: 80%;} .bl-source div{white-space: pre;font-family: monospace;} .bl-source > div > span:first-child{background-color: #EAEAEA;color: #949494;display: inline-block;padding: 0 10px;text-align: center;width: 30px;} .bl-source .miss{background-color:#e6c3c7} .bl-source span.branchWarning{color:#000;background-color:yellow;} .bl-source span.branchOkay{color:#000;background-color:transparent;}",
+
+                    output = doc_template.replace(/{{insert_styles}}/, styles);
+                    output = output.replace(/{{insert_script}}/, blanket_toggleSource.toString());
+                    output = output.replace(/{{insert_report}}/, reportHTML);
+                    if (typeof window.callPhantom === 'function') {
+                        window.callPhantom({ type: 'reportHtml', html: output });
+                    }
+
+                    function blanket_toggleSource(id) {
+                        var element = document.getElementById(id);
+                        if(element.style.display === 'block') {
+                            element.style.display = 'none';
+                        } else {
+                            element.style.display = 'block';
+                        }
+                    }
+                }
             }
         }
     });
